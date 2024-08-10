8bf8f2f1fd0cc3ea6d3d544e13c09337b6fee168
areum0116 <areum0116@naver.com>
2024-08-10 15:33:45 +0900

    init

---
+++ shcedule-management/src/main/java/com/example/shcedulemanagement/ShceduleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.shcedulemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShceduleManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShceduleManagementApplication.class, args);
+    }
+
+}
---
+++ shcedule-management/src/test/java/com/example/shcedulemanagement/ShceduleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shcedulemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShceduleManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
