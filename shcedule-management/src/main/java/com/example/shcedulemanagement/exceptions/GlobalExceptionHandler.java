afc0a5925ef7ef7d2c7cd92f69eb3d6bfb664174
areum0116 <areum0116@naver.com>
2024-08-14 16:44:03 +0900

    [feat] Add GlobalExceptionHandler using @ExceptionHandler

---
+++ shcedule-management/src/main/java/com/example/shcedulemanagement/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.shcedulemanagement.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleAllException(Exception ex) {
+        return new ResponseEntity<>("An error occurred : " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
